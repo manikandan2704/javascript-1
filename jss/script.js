b83ec4e0e21837d41b67576dad3217d5908f604e
manikandan <manimani19508@gmail.com>
2022-08-24 16:28:10 +0530

    add new file

---
+++ jss/script.js
@@ -0,0 +1,19 @@
+/*let a=10;
+let b=13;
+console.log(a+b);*/
+
+//calculate area of circle;//
+
+/*let A=Math.PI*Math.pow(7,2);
+console.log(A);*/
+
+/*calculate area of traingle;*/
+
+/*let A=1/2*6*8;
+console.log(A);*/
+
+/*simple Interest;*/
+
+/*let A=8*8*2/100
+console.log(A);*/
+1` `
