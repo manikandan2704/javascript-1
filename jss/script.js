a5b122689b9e73c9d31c7efcc9f58d81434031c0
manikandan <manimani19508@gmail.com>
2022-09-06 09:16:58 +0530

    add new file

--- jss/script.js
+++ jss/script.js
@@ -4,7 +4,7 @@ console.log(a+b);*/
 
 //calculate area of circle;//
 
-/*let A=Math.PI*Math.pow(7,2);
+/*let A=Math.PI*Math.pow(7,2); 
 console.log(A);*/
 
 /*calculate area of traingle;*/
@@ -16,4 +16,86 @@ console.log(A);*/
 
 /*let A=8*8*2/100
 console.log(A);*/
-1` `
+/*
+var marks=[];
+marks.push(44);
+marks.push(44);
+marks.push(44);
+marks.push(44);
+marks.push(44);
+console.log(marks);*/
+ 
+//pop method//
+
+/*var fruits=["mango","banana","apple"];
+fruits.pop();
+console.log(fruits);*/
+
+//shift method//
+
+/*var mycars=["rols","bmw","thor"];
+mycars.shift("thor");
+console.log(mycars);*/
+
+//slice method//
+
+/*var subject=["tamil","maths","english","history"]
+subject.slice();
+console.log(subject);*/
+
+//splice method//
+ /*var software=["html","css","javascript","python","c",]
+ software.splice();
+ console.log(software);*/
+
+ //tostring method//
+ /*var software=["html","css","javascript","python","c",]
+ software.toString();
+ console.log(software);*/
+
+ //unshift method//
+ /*var software=[]
+ software.unshift(10);
+ software.unshift(10);
+ software.unshift(10);
+ software.unshift(10);
+ software.unshift(10);
+ console.log(software)*\
+
+
+
+ /*Program for calculating Cube
+ let A=Math.cbrt(125);
+ console.log(A);*\
+
+ /*program for square 
+ let A=Math.sqrt(4);
+ console.log(A);*/
+
+ /*Program to SWAP two numbers
+
+ let A=25;
+ let B=87;
+ let c=166;
+ console.log(' the value after swapping')*/
+   
+
+//  let a=10;
+//  let b=45;
+//  console.log(a<b);
+  
+
+//  num=10;
+//  if
+function print(){
+    var input = document.getElementById("input");
+    console.log(input.value);
+
+    var output = document.createElement("P");
+    output.innerText = input.value;
+    document.getElementsByTagName("BODY")[0].appendChild(output);
+
+    input.value = "";
+}
+
+ 
